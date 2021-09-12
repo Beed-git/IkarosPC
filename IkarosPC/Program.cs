4c0bb70768f3dff679d902d8a02548a77116cf6b
bp66 <bp66@waikato.ac.nz>
2021-09-12 17:48:18 +1200

    Removed test program.

--- IkarosPC/Program.cs
+++ IkarosPC/Program.cs
@@ -13,30 +13,6 @@ namespace IkarosPC
         static void Main(string[] args)
         {
             _memory = new Memory();
-
-            _memory.SetInitialMemory(new ushort[]
-            {
-                // Address of ram-switcher.
-                0x1300, 0xFFFF,
-                // Screen size.
-                0x1310, 0x5A00,
-                // Yellow
-                0x1320, 0xFF00,
-                // Store 2 in ram-switch
-                0x1400, 0x0002,
-                // Write yellow at address
-                0x1221,
-                // B -= 1
-                0x2310, 0x0001,
-                0x1081,
-                // Branch if not zero
-                0xF010, 0x0008,
-                // Final write yellow at address
-                0x1221,
-                // Stop
-                0x0100
-            });
-
             _cpu = new CPU(_memory);
 
             var display = new Display(_memory.Vram);
