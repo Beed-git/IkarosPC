90e249499090b6b66dc78fb58c60bf6c637e1a10
bp66 <bp66@waikato.ac.nz>
2021-09-14 13:54:41 +1200

    Rewrote half of move tests.

--- IkarosPC.Tests/InstructionTests/MoveInstructionsTests.cs
+++ IkarosPC.Tests/InstructionTests/MoveInstructionsTests.cs
@@ -22,149 +22,21 @@ namespace IkarosPC.Tests.InstructionTests
 
             SetupHelpers.CheckInitialCPUState(_cpu);
         }
-        // 0x13
-        [Test]
-        public void TestMoveLiteralToRegister()
-        {
-            _memory.SetInitialMemory(new ushort[]
-            {
-                // Store 0x1234 in Register A
-                0x1300, 0x1234,
-                // Store 0x4321 in Register Z
-                0x1370, 0x4321,
-                // Store 0xFEDC in Register B
-                // 0x000F at end which shouldn't affect the execution.
-                0x131F, 0xFEDC,
-                // Store 0x5432 in Register A
-                0x1300, 0x5432
-            });
-
-            _cpu.Step();
-            Assert.IsTrue(_cpu.Registers.PC == 2);
-            Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.Z == 0);
-            Assert.IsTrue(_cpu.Registers.B == 0);
-
-            _cpu.Step();
-            Assert.IsTrue(_cpu.Registers.PC == 4);
-            Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.Z == 0x4321);
-            Assert.IsTrue(_cpu.Registers.B == 0);
-
-            _cpu.Step();
-            Assert.IsTrue(_cpu.Registers.PC == 6);
-            Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.Z == 0x4321);
-            Assert.IsTrue(_cpu.Registers.B == 0xFEDC);
-
-            _cpu.Step();
-            Assert.IsTrue(_cpu.Registers.PC == 8);
-            Assert.IsTrue(_cpu.Registers.A == 0x5432);
-            Assert.IsTrue(_cpu.Registers.Z == 0x4321);
-            Assert.IsTrue(_cpu.Registers.B == 0xFEDC);
-            
-        }
-
-        // 0x14
-        [Test]
-        public void TestMoveLiteralToMemoryAddress()
-        {
-            _memory.SetInitialMemory(new ushort[]
-            {
-                // Store 0x0010 into Register A, 0x1234 into Register B.
-                0x1300, 0x0010,
-                0x1310, 0x1234,
-                // Store 0xF1F2 into memory specified by Register A
-                0x1400, 0xF1F2,
-                // Store 0x1515 into memory specified by Register B
-                0x1419, 0x1515,
-            });
-
-            Assert.IsTrue(_memory[0x0010] == 0);
-            Assert.IsTrue(_memory[0x1234] == 0);
-
-            _cpu.Step();
-            _cpu.Step();
-
-            Assert.IsTrue(_cpu.Registers.PC == 4);
-            Assert.IsTrue(_cpu.Registers.A == 0x0010);
-            Assert.IsTrue(_cpu.Registers.B == 0x1234);
-            Assert.IsTrue(_memory[0x0010] == 0);
-            Assert.IsTrue(_memory[0x1234] == 0);
-
-            _cpu.Step();
-
-            Assert.IsTrue(_cpu.Registers.PC == 6);
-            Assert.IsTrue(_cpu.Registers.A == 0x0010);
-            Assert.IsTrue(_cpu.Registers.B == 0x1234);
-            Assert.IsTrue(_memory[0x0010] == 0xF1F2);
-            Assert.IsTrue(_memory[0x1234] == 0);
-
-            _cpu.Step();
-
-            Assert.IsTrue(_cpu.Registers.PC == 8);
-            Assert.IsTrue(_cpu.Registers.A == 0x0010);
-            Assert.IsTrue(_cpu.Registers.B == 0x1234);
-            Assert.IsTrue(_memory[0x0010] == 0xF1F2);
-            Assert.IsTrue(_memory[0x1234] == 0x1515);
-        }
-
-        [Test]
-        public void MoveLiteralToLiteral()
-        {
-            _memory.SetInitialMemory(new ushort[] {
-                // Store 0x1234 at address 0x2000.
-                0x1500, 0x1234, 0x2000,
-                // Switch to vram
-                0x1500, 0x0002, 0xFFFF,
-                // Store in vram
-                0x1500, 0x4321, 0x1000,
-            });
-
-            // Manually set vram at 0x1000 to zero, since it could aready contain a value. 
-            _memory.Vram[0x1000] = 0;
-
-            Assert.IsTrue(_memory.Ram[0x2000] == 0);
-            Assert.IsTrue(_memory.Vram[0x1000] == 0);
-            Assert.IsTrue(_memory[0xFFFF] == 0);
-
-            _cpu.Step();
-
-            Assert.IsTrue(_cpu.Registers.PC == 3);
-            Assert.IsTrue(_memory.Ram[0x2000] == 0x1234);
-            Assert.IsTrue(_memory.Vram[0x1000] == 0);
-            Assert.IsTrue(_memory[0xFFFF] == 0);
-
-            _cpu.Step();
-
-            Assert.IsTrue(_cpu.Registers.PC == 6);
-            Assert.IsTrue(_memory.Ram[0x2000] == 0x1234);
-            Assert.IsTrue(_memory.Vram[0x1000] == 0);
-            Assert.IsTrue(_memory[0xFFFF] == 0x0002);
-
-            _cpu.Step();
-
-            Assert.IsTrue(_cpu.Registers.PC == 9);
-            Assert.IsTrue(_memory.Ram[0x2000] == 0x1234);
-            Assert.IsTrue(_memory.Vram[0x1000] == 0x4321);
-            Assert.IsTrue(_memory[0xFFFF] == 0x0002);
-        }
-
         // 0x10
         [Test]
-        public void MoveFromRegisterToRegister()
+        public void TestMoveRegisterToRegister()
         {
             _memory.SetInitialMemory(new ushort[]
             {
-                // Set Register A to 0x1234, Register B to 0x5432.
-                0x1300, 0x1234,
-                0x1310, 0x5432,
-                // Move from Register A to Register C
+                // Load values into registers.
+                0x1100, 0x1234,
+                0x1110, 0x4321,
+                // Move into empty register.
                 0x1002,
-                // Move from Register B to register A
+                // Overwrite register.
                 0x1010,
-                // Move from Register C to register B
-                0x1021
+                // Test invalid registers.
+                0x10FF,
             });
 
             _cpu.Step();
@@ -172,151 +44,289 @@ namespace IkarosPC.Tests.InstructionTests
 
             Assert.IsTrue(_cpu.Registers.PC == 4);
             Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.B == 0x5432);
+            Assert.IsTrue(_cpu.Registers.B == 0x4321);
             Assert.IsTrue(_cpu.Registers.C == 0);
 
             _cpu.Step();
 
             Assert.IsTrue(_cpu.Registers.PC == 5);
             Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.B == 0x5432);
+            Assert.IsTrue(_cpu.Registers.B == 0x4321);
             Assert.IsTrue(_cpu.Registers.C == 0x1234);
 
             _cpu.Step();
 
             Assert.IsTrue(_cpu.Registers.PC == 6);
-            Assert.IsTrue(_cpu.Registers.A == 0x5432);
-            Assert.IsTrue(_cpu.Registers.B == 0x5432);
+            Assert.IsTrue(_cpu.Registers.A == 0x4321);
+            Assert.IsTrue(_cpu.Registers.B == 0x4321);
             Assert.IsTrue(_cpu.Registers.C == 0x1234);
 
-            _cpu.Step();
-
-            Assert.IsTrue(_cpu.Registers.PC == 7);
-            Assert.IsTrue(_cpu.Registers.A == 0x5432);
-            Assert.IsTrue(_cpu.Registers.B == 0x1234);
-            Assert.IsTrue(_cpu.Registers.C == 0x1234);
+            // Test invalid register.
+            Assert.Throws<IndexOutOfRangeException>(_cpu.Step);
         }
 
         // 0x11
         [Test]
-        public void MoveFromMemoryToRegister()
+        public void TestMovei16ToRegister()
         {
             _memory.SetInitialMemory(new ushort[]
             {
-                // Init registers
-                0x1300, 0x0007,
-                0x1310, 0x0008,
-                // Move from memory[7] to C
-                0x1102,
-                // Move from memory[8] to D
-                0x1113,
-                // Move from memory[7] to A
-                0x1100,
-                // Data
-                0x1234, 0x5432
+                // Put 0x1234 in A.
+                0x1100, 0x1234,
+                // Put 0x1212 in B.
+                0x1110, 0x1212,
+                // Put 0x4321 in A.
+                0x1100, 0x4321,
+                // Put 0x1111 in C.
+                0x112F, 0x1111,
+                // Invalid instruction call.
+                0x11FF, 0x1111,
             });
 
-            Assert.IsTrue(_memory[0x0007] == 0x1234);
-            Assert.IsTrue(_memory[0x0008] == 0x5432);
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 2);
+            Assert.IsTrue(_cpu.Registers.A == 0x1234);
+            Assert.IsTrue(_cpu.Registers.B == 0);
+            Assert.IsTrue(_cpu.Registers.C == 0);
+
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 4);
+            Assert.IsTrue(_cpu.Registers.A == 0x1234);
+            Assert.IsTrue(_cpu.Registers.B == 0x1212);
+            Assert.IsTrue(_cpu.Registers.C == 0);
+
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 6);
+            Assert.IsTrue(_cpu.Registers.A == 0x4321);
+            Assert.IsTrue(_cpu.Registers.B == 0x1212);
+            Assert.IsTrue(_cpu.Registers.C == 0);
+
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 8);
+            Assert.IsTrue(_cpu.Registers.A == 0x4321);
+            Assert.IsTrue(_cpu.Registers.B == 0x1212);
+            Assert.IsTrue(_cpu.Registers.C == 0x1111);
+
+            // Test invalid register.
+            Assert.Throws<IndexOutOfRangeException>(_cpu.Step);
+        }
+
+        //0x12
+        [Test]
+        public void TestMoveFromMemoryAtRegisterToRegister()
+        {
+            _memory.SetInitialMemory(new ushort[]
+            {
+                // Init registers.
+                0x1100, 0x1000,
+                0x1110, 0x2000,
+                // Move from (0x1000) into register C
+                0x1202,
+                // Overwrite value.
+                0x1212,
+                // Overwrite self.
+                0x1211,
+                // Invalid registers.
+                0x12FF
+            });
+
+            _memory.Ram[0x1000] = 0x1234;
+            _memory.Ram[0x2000] = 0xFEFE;
 
             _cpu.Step();
             _cpu.Step();
 
             Assert.IsTrue(_cpu.Registers.PC == 4);
-            Assert.IsTrue(_cpu.Registers.A == 0x0007);
-            Assert.IsTrue(_cpu.Registers.B == 0x0008);
+            Assert.IsTrue(_cpu.Registers.A == 0x1000);
+            Assert.IsTrue(_cpu.Registers.B == 0x2000);
             Assert.IsTrue(_cpu.Registers.C == 0);
-            Assert.IsTrue(_cpu.Registers.D == 0);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0x1234);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0xFEFE);
 
             _cpu.Step();
 
             Assert.IsTrue(_cpu.Registers.PC == 5);
-            Assert.IsTrue(_cpu.Registers.A == 0x0007);
-            Assert.IsTrue(_cpu.Registers.B == 0x0008);
+            Assert.IsTrue(_cpu.Registers.A == 0x1000);
+            Assert.IsTrue(_cpu.Registers.B == 0x2000);
             Assert.IsTrue(_cpu.Registers.C == 0x1234);
-            Assert.IsTrue(_cpu.Registers.D == 0);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0x1234);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0xFEFE);
 
             _cpu.Step();
 
             Assert.IsTrue(_cpu.Registers.PC == 6);
-            Assert.IsTrue(_cpu.Registers.A == 0x0007);
-            Assert.IsTrue(_cpu.Registers.B == 0x0008);
-            Assert.IsTrue(_cpu.Registers.C == 0x1234);
-            Assert.IsTrue(_cpu.Registers.D == 0x5432);
+            Assert.IsTrue(_cpu.Registers.A == 0x1000);
+            Assert.IsTrue(_cpu.Registers.B == 0x2000);
+            Assert.IsTrue(_cpu.Registers.C == 0xFEFE);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0x1234);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0xFEFE);
 
             _cpu.Step();
 
             Assert.IsTrue(_cpu.Registers.PC == 7);
+            Assert.IsTrue(_cpu.Registers.A == 0x1000);
+            Assert.IsTrue(_cpu.Registers.B == 0xFEFE);
+            Assert.IsTrue(_cpu.Registers.C == 0xFEFE);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0x1234);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0xFEFE);
+
+            // Test invalid register.
+            Assert.Throws<IndexOutOfRangeException>(_cpu.Step);
+
+        }
+        //0x13
+        [Test]
+        public void TestMoveFromMemoryAti16ToRegister()
+        {
+            _memory.SetInitialMemory(new ushort[]
+{
+                // Init register with random values.
+                0x1100, 0x1234,
+                // Move from (0x1000) into register B
+                0x1310, 0x1000,
+                // Overwrite value.
+                0x1300, 0x2000,
+                // Invalid register.
+                0x13FF
+            });
+
+            _memory.Ram[0x1000] = 0xDDDD;
+            _memory.Ram[0x2000] = 0x8383;
+
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 2);
             Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.B == 0x0008);
-            Assert.IsTrue(_cpu.Registers.C == 0x1234);
-            Assert.IsTrue(_cpu.Registers.D == 0x5432);
+            Assert.IsTrue(_cpu.Registers.B == 0);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0xDDDD);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0x8383);
+
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 4);
+            Assert.IsTrue(_cpu.Registers.A == 0x1234);
+            Assert.IsTrue(_cpu.Registers.B == 0xDDDD);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0xDDDD);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0x8383);
+
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 6);
+            Assert.IsTrue(_cpu.Registers.A == 0x8383);
+            Assert.IsTrue(_cpu.Registers.B == 0xDDDD);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0xDDDD);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0x8383);
+
+            // Test invalid register.
+            Assert.Throws<IndexOutOfRangeException>(_cpu.Step);
         }
 
-        // 0x12
+        //0x14
         [Test]
-        public void MoveFromRegisterToMemory()
+        public void TestMoveFromRegisterToMemoryAtRegister()
+        {
+            _memory.SetInitialMemory(new ushort[] {
+                // Init registers.
+                0x1100, 0x1234,
+                0x1110, 0x1000,
+                // Store 0x1234 at (0x1000).
+                0x1401,
+                // Overwrite with self.
+                0x1411,
+                // Invalid registers.
+                0x14FF
+            });
+
+            _memory.Ram[0x1000] = 0;
+
+            _cpu.Step();
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 4);
+            Assert.IsTrue(_cpu.Registers.A == 0x1234);
+            Assert.IsTrue(_cpu.Registers.B == 0x1000);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0);
+
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 5);
+            Assert.IsTrue(_cpu.Registers.A == 0x1234);
+            Assert.IsTrue(_cpu.Registers.B == 0x1000);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0x1234);
+
+            _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 6);
+            Assert.IsTrue(_cpu.Registers.A == 0x1234);
+            Assert.IsTrue(_cpu.Registers.B == 0x1000);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0x1000);
+
+            // Test invalid register.
+            Assert.Throws<IndexOutOfRangeException>(_cpu.Step);
+        }
+
+        //0x15
+        [Test]
+        public void TestMovei16IntoMemoryAtRegister()
         {
             _memory.SetInitialMemory(new ushort[]
             {
-                // Init registers.
-                0x1300, 0x1234,
-                0x1310, 0x5432,
-                0x1320, 0x0010,
-                0x1330, 0x0011,
-                // Move from Register A to memory specified by Register C.
-                0x1202,
-                // Move from Register B to memory specified by Register D.
-                0x1213,
-                // Move from Register C to memory specified by Register D.
-                0x1223
+                // Load values into registers.
+                0x1100, 0x1000,
+                0x1110, 0x2000,
+                // Store 0x1234 at 0x1000.
+                0x1500, 0x1234,
+                // Store 0x4321 at 0x2000.
+                0x1510, 0x4321,
+                // Overwrite 0x1000.
+                0x1500, 0xFFFF,
+                // Invalid register.
+                0x15FF, 0x1234
             });
 
-            Assert.IsTrue(_memory[0x0010] == 0);
-            Assert.IsTrue(_memory[0x0011] == 0);
+            // Ensure memory starts at 0.
+            _memory.Ram[0x1000] = 0;
+            _memory.Ram[0x2000] = 0;
 
             _cpu.Step();
             _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 4);
+            Assert.IsTrue(_cpu.Registers.A == 0x1000);
+            Assert.IsTrue(_cpu.Registers.B == 0x2000);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0);
+
             _cpu.Step();
+
+            Assert.IsTrue(_cpu.Registers.PC == 6);
+            Assert.IsTrue(_cpu.Registers.A == 0x1000);
+            Assert.IsTrue(_cpu.Registers.B == 0x2000);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0x1234);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0);
+
             _cpu.Step();
 
             Assert.IsTrue(_cpu.Registers.PC == 8);
-            Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.B == 0x5432);
-            Assert.IsTrue(_cpu.Registers.C == 0x0010);
-            Assert.IsTrue(_cpu.Registers.D == 0x0011);
-            Assert.IsTrue(_memory[0x0010] == 0);
-            Assert.IsTrue(_memory[0x0011] == 0);
-
-            _cpu.Step();
-
-            Assert.IsTrue(_cpu.Registers.PC == 9);
-            Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.B == 0x5432);
-            Assert.IsTrue(_cpu.Registers.C == 0x0010);
-            Assert.IsTrue(_cpu.Registers.D == 0x0011);
-            Assert.IsTrue(_memory[0x0010] == 0x1234);
-            Assert.IsTrue(_memory[0x0011] == 0);
+            Assert.IsTrue(_cpu.Registers.A == 0x1000);
+            Assert.IsTrue(_cpu.Registers.B == 0x2000);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0x1234);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0x4321);
 
             _cpu.Step();
 
             Assert.IsTrue(_cpu.Registers.PC == 10);
-            Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.B == 0x5432);
-            Assert.IsTrue(_cpu.Registers.C == 0x0010);
-            Assert.IsTrue(_cpu.Registers.D == 0x0011);
-            Assert.IsTrue(_memory[0x0010] == 0x1234);
-            Assert.IsTrue(_memory[0x0011] == 0x5432);
-
-            _cpu.Step();
-
-            Assert.IsTrue(_cpu.Registers.PC == 11);
-            Assert.IsTrue(_cpu.Registers.A == 0x1234);
-            Assert.IsTrue(_cpu.Registers.B == 0x5432);
-            Assert.IsTrue(_cpu.Registers.C == 0x0010);
-            Assert.IsTrue(_cpu.Registers.D == 0x0011);
-            Assert.IsTrue(_memory[0x0010] == 0x1234);
-            Assert.IsTrue(_memory[0x0011] == 0x0010);
+            Assert.IsTrue(_cpu.Registers.A == 0x1000);
+            Assert.IsTrue(_cpu.Registers.B == 0x2000);
+            Assert.IsTrue(_memory.Ram[0x1000] == 0xFFFF);
+            Assert.IsTrue(_memory.Ram[0x2000] == 0x4321);
+            
+            // Test invalid register.
+            Assert.Throws<IndexOutOfRangeException>(_cpu.Step);
         }
     }
 }
